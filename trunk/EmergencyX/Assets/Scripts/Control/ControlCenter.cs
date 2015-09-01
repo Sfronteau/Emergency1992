7a6dfeee24ca301ec46c8926da42ea455d0f0a57
SamF <sfronteau@gmail.com>
2015-08-31 21:37:06 -0400

    intial commit

---
+++ trunk/EmergencyX/Assets/Scripts/Control/ControlCenter.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ControlCenter : MonoBehaviour 
+{
+	public List<GameObject> PhonesIdle = new List<GameObject>();
+	public List<GameObject> PhonesCall = new List<GameObject>();
+	public GameObject PhoneAnswered;
+	public GameObject PhoneMiddle;
+
+	public float TimerTime;
+	public float TimerCall = 1;
+
+	public int TimerMin = 3;
+	public int TimerMax = 25;
+
+	GameObject PhoneSelect;
+
+	Buttons ButtonsScript;
+
+	public static ControlCenter Instance;
+	
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (PhonesIdle.Capacity > 0)
+		{
+			TimerTime += Time.deltaTime;
+		}
+
+		if (TimerTime >= TimerCall) 
+		{
+			PhoneSelect = PhonesIdle[Random.Range(0, PhonesIdle.Capacity)];
+			TimerCall = Random.Range(TimerMin,TimerMax);
+			ButtonsScript = PhoneSelect.GetComponent<Buttons>();
+			PhonesIdle.Remove(PhoneSelect);
+			PhonesCall.Add(PhoneSelect);
+			ButtonsScript.State = "Call";
+			TimerTime = 0;
+		}	
+	}
+
+	public void HungUp()
+	{
+		ButtonsScript = PhoneAnswered.GetComponent<Buttons>();
+		ButtonsScript.LineHungUp ();
+		PhoneAnswered = PhoneMiddle;
+	}
+}
---
+++ trunk/EmergencyX/Assets/Scripts/Phone/Buttons.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Buttons : MonoBehaviour 
+{
+	public string State = "Idle";
+
+	public GameObject Timer;
+
+	public bool HasBeenHolded;
+	public bool Timed;
+	public bool Called = false; 
+
+	public float TimerTimeBase;
+	public float TimerTime;
+	
+	Animator Anim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Anim = GetComponent<Animator> ();
+		Timer = this.gameObject.transform.GetChild(1).gameObject;
+	}
+
+	void FixedUpdate ()
+	{
+		Timer.GetComponent<Image> ().fillAmount = TimerTime / TimerTimeBase;
+
+		switch (State)
+		{
+		case"Idle":
+			TimerTime = TimerTimeBase;
+			Anim.SetBool("IsCalled",false);
+			Anim.SetBool("HangUp",true);
+			Timer.SetActive(false);
+			break;
+		case "Answered":
+			Timer.SetActive(false);
+			Anim.SetBool("HasAnswered",true);
+			Anim.SetBool("IsCalled",false);
+			Anim.SetBool("OnHold", false);
+			if (ControlCenter.Instance.PhoneAnswered == null)
+			{
+				ControlCenter.Instance.PhoneAnswered = this.gameObject;
+			}
+			else if (ControlCenter.Instance.PhoneAnswered != this.gameObject)
+			{
+				ControlCenter.Instance.PhoneMiddle = this.gameObject;
+				ControlCenter.Instance.HungUp();
+			}
+			ControlCenter.Instance.PhonesIdle.Remove(this.gameObject);
+			break;
+		case "Call":
+			Anim.SetBool("HangUp",false);
+			Anim.SetBool("IsCalled",true);
+			Timer.SetActive(true);
+			TimerTime -= Time.deltaTime;
+			if (TimerTime <= 0)
+			{
+				LineHungUp();
+			}
+			break;
+		case "Hold":
+			Timer.SetActive(true);
+			Anim.SetBool("HasAnswered",false);
+			Anim.SetBool("OnHold",true);
+			ControlCenter.Instance.PhoneAnswered = null;
+			if(HasBeenHolded)
+			{
+				TimerTime -= Time.deltaTime;
+				if (TimerTime <= 0)
+				{
+					LineHungUp();
+				}
+			}
+			else
+			{
+				HasBeenHolded = true;
+				TimerTime = TimerTimeBase;
+			}
+			break;
+		}
+	}
+
+	public void ButtonClick()
+	{
+		switch (State)
+		{
+		case "Call":
+			State = "Answered";
+			break;
+		case "Answered":
+			State = "Hold";
+			break;
+		case "Hold":
+			State = "Answered";
+			break;
+		}
+	}
+
+	public void LineHungUp()
+	{
+		State = "Idle";
+		ControlCenter.Instance.PhoneAnswered = null;
+		ControlCenter.Instance.PhonesIdle.Add(this.gameObject);
+		ControlCenter.Instance.PhonesCall.Remove(this.gameObject);
+	}
+}
